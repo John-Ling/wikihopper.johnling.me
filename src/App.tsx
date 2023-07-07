19a18117eed660c32e947dba668782b9e9524543
John Ling <johnlingbusiness@gmail.com>
2023-07-07 17:04:07 +1000

    Added basic test button to swap two wiki articles.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     const [titles, setTitles] = useState<string[]>([]);
     const [wikiData, setWikiData] = useState<WikipediaData[]>([]);
     const [hopCount, setHopCount] = useState(10);
+    const [state, setState] = useState(false);
 
 
     let hopIcons: HopIcon[] = []
@@ -19,34 +20,12 @@ function App() {
     hopIcons[0].active = true;
 
     useEffect(() => { // Create two random wikipedia titles
-        console.log("First");
         const generate_wikipedia_titles = async (n: number): Promise<any> => { // Generates n random wikipedia titles
             const API_ENDPOINT: string = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=${n}&origin=*`;
             let data: Response = await fetch(API_ENDPOINT, { method: "GET" });
             return data.json();
         }
 
-        let blocked: boolean = false;
-        generate_wikipedia_titles(2)
-            .then(response => {
-                let titles = response.query.random;
-                let tmp: string[] = [];
-                titles.forEach((obj: any) => {
-                    if (!blocked) {
-                        console.log(obj.title);
-                        tmp.push(obj.title);
-                    }
-                })
-                setTitles(tmp);
-            })
-            .catch(error => console.log(error));
-        return () => {
-            blocked = true;
-        }
-    }, []);
-
-    useEffect(() => { // Create wikipedia data using those titles
-        console.log("Second");
         const fetch_wikipedia_data = async (title: string): Promise<WikipediaData> => {
             const API_ENDPOINT: string = `https://en.wikipedia.org/w/api.php?action=parse&page=${title}&format=json&origin=*`;
             let data: Response = await fetch(API_ENDPOINT, { method: "GET" });
@@ -61,25 +40,41 @@ function App() {
         }
 
         let blocked: boolean = false;
-        titles.forEach((title: string) => {
-            if (!blocked) { 
-                get_wikipedia_data(title)?.then(data => {
-                    console.log(data);
-                    setWikiData([...wikiData, data]);
-                });
-            }
-        })
+        generate_wikipedia_titles(2)
+            .then(response => {
+                let titles = response.query.random;
+                let tmp: string[] = [];
+                titles.forEach((obj: any) => {
+                    if (!blocked) {
+                        tmp.push(obj.title);
+                    }
+                })
+                setTitles(tmp);
+                //console.log(titles);
+                titles.forEach((title: {title: string}) => {
+                    console.log(title);
+                    if (!blocked) {
+                        let searchTitle: string = title.title;
+                        console.log(`Getting Wikipedia data for ${searchTitle}`);
+                        get_wikipedia_data(searchTitle)?.then(data => setWikiData(previous => [...previous, data]));
+                    }
+                })
+            })
+            .catch(error => console.log(error));
+        
         return () => {
+            console.log("First completed");
             blocked = true;
         }
     }, []);
-    
+
     return (
         <>
             <HopBar hopIcons={ hopIcons }></HopBar>
             <div className="main-view">
                 {/* {visibility ? { wikiData ? <WikiFrame wikiData={wikiData[0]}/> : <p>Loading</p>} : <p>Hello</p>} */}
-                { wikiData.map((data: WikipediaData) => <div dangerouslySetInnerHTML={{ __html: data.parse.text['*']}}/>)}
+                {/* { wikiData.map((data: WikipediaData) => <div dangerouslySetInnerHTML={{ __html: data.parse.text['*']}}/>)} */}
+                <WikiFrame wikiData={wikiData}/>
                 {/* {wikiData ? <div dangerouslySetInnerHTML={{ __html: wikiData.parse.text['*'] }} /> : <p>Loading</p>} */}
             </div>
         </>
--- src/components/WikiFrame.tsx
+++ src/components/WikiFrame.tsx
@@ -5,15 +5,16 @@ import "../css/wiki_frame.css";
 export const WikiFrame: React.FC<{ wikiData: WikipediaData[] }> = ({ wikiData }) => {
 	const [visibility, setVisiblity] = useState(true); // Determines which iframe is shown (true or 1 = 1st | false or 0 = 2nd)
 
-	console.log(wikiData.length);
-	let displayData = wikiData.map((data: WikipediaData) => {
-		console.log("Mapped data");
-		console.log(data);
-	});
-
+	console.log(wikiData);
+	//let displayData = wikiData.map((data: WikipediaData) => { <div dangerouslySetInnerHTML={{ __html: data.parse.text['*'] }}/> });
+	let displayData = wikiData.map((data: WikipediaData) => <div class="wiki-frame" dangerouslySetInnerHTML={{__html: data.parse.text['*']}}/>);
+	let wikiFrameA = displayData[0];
+	let wikiFrameB = displayData[1];
 	return (
 		<>
 			{/* {wikiData ? <div dangerouslySetInnerHTML={{ __html: wikiData.parse.text['*'] }} /> : <p>Loading</p>} */}
+			{ visibility ? wikiFrameA : wikiFrameB }
+			<button onClick={() => setVisiblity(!visibility)}>Click me</button>
 		</>
 	);
 }
