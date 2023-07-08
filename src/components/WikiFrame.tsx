8fbda57e539a8f29ba62eca81182b656b38269d1
John Ling <johnlingbusiness@gmail.com>
2023-07-08 21:21:58 +1000

    Reverting to go back to html instead of iframe

--- src/components/WikiFrame.tsx
+++ src/components/WikiFrame.tsx
@@ -1,15 +1,15 @@
+import { useState } from "react";
+import { WikipediaData } from "../types";
 import "../css/wiki_frame.css";
+import "../css/wikipedia.css";
 
-export const WikiFrame: React.FC<{ titles: string[], visible: boolean }> = ({ titles, visible }) => {
-	let displayData = titles.map((title: string) => 
-		<iframe className="wiki-frame" src={`https://en.wikipedia.org/wiki/${title.replace(/ /g, '_')}`} title={title}></iframe>
-	);
+export const WikiFrame: React.FC<{ wikiData: WikipediaData[], visible: boolean }> = ({ wikiData, visible }) => {
+	let displayData = wikiData.map((data: WikipediaData) => <div className="wiki-frame" dangerouslySetInnerHTML={{__html: data.parse.text['*']}}/>);
 	let wikiFrameA = displayData ? displayData[0] : <p>Loading</p>;
 	let wikiFrameB = displayData ? displayData[1] : <p>Loading</p>;
-
 	return (
 		<>
-			{visible ? wikiFrameA : wikiFrameB}
+			{ visible ? wikiFrameA : wikiFrameB }
 		</>
 	);
 }
